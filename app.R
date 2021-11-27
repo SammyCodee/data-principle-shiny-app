2a606e492f9836da42bbdea90849ff0a19f3ab7e
SAM <samazingg96@gmail.com>
2021-11-27 19:06:40 +0800

    Added basic css.

--- app.R
+++ app.R
@@ -1,7 +1,32 @@
 library(shiny)
+library(shinyWidgets)
 
-ui <- fluidPage("heyhey")
+ui <- fluidPage(
+  setBackgroundColor(
+    color = c("#ffffcc", "#003399"),
+    gradient = "linear",
+    direction = "bottom"
+  ),
+  titlePanel("Student Dropout Rates"),
+  
+  fluidRow(
+    navlistPanel(
+      "Menu",
+      tabPanel(
+        title = "Component 1"
+      ),
+      tabPanel(
+        title = "Component 2"
+      )
+    )
+      
+      
+      
+  )
+)
 
-server <- function(input, output) {}
+server <- function(input, output) {
+  
+}
 
 shinyApp(ui = ui, server = server)
